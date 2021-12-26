31f3bee587486fbdd71f6239996d5b3abc200a8e
yihuilu <yihuilu@tencent.com>
2021-12-26 17:18:43 +0800

    feat(会车场景): 实现了会车场景的可视化
    1、红蓝两辆车，其对应的当前坐标、目标坐标、方向角和速度
    2、车端和监控端各一个页面，车端后缀为lite
    3、回车页面实现为独立页面，用iframe拉入决策页面
    4、信息发送使用决策页面的组件进行，车端使用轮询方式查取数据

--- static/js/server.js
+++ static/js/server.js
@@ -2,3 +2,4 @@
 // var server_ip = 'http://202.120.60.31:80/zc'    // as-lab-0 mr.Zeng Nginx
 // var server_ip = 'http://202.120.60.31:2222/'
 var server_ip = 'http://47.103.85.106:80/datas/'    // Aliyun mr.Zeng Nginx
+var server_ip_car = 'http://47.103.85.106:80/car_meet/'    // Aliyun mr.Zeng Nginx zc 会车
